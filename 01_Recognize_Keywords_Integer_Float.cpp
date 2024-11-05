a4f0e35f312c93572c314178d6645789f31d30e6
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-11-05 11:58:53 +0600

    added

--- 01_Recognize_Keywords_Integer_Float.cpp
+++ 01_Recognize_Keywords_Integer_Float.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+// Function to check if a given string is a keyword
+bool is_keyword(const string& str) {
+    const string keywords[] = {"int", "float", "if", "then", "else", "for", "printf", "switch", "case", "return"};
+    for (int i = 0; i < 10; i++) {
+        if (str == keywords[i]) {
+            return true;
+        }
+    }
+    return false;
+}
