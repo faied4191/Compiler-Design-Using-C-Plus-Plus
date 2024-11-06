9f3dec87b0bc4a66e942a30232636e2fd1051b8a
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-11-06 08:52:22 +0600

    added

--- 01_Recognize_Keywords_Integer_Float.cpp
+++ 01_Recognize_Keywords_Integer_Float.cpp
@@ -23,19 +23,17 @@ bool is_integer(const string& str) {
     return true;
 }
 
-// Main function
-int main() {
-    string input;
-    cout << "Enter input: ";
-    cin >> input;
-
-    if (is_keyword(input)) {
-        cout << "Keyword" << endl;
-    } else if (is_integer(input)) {
-        cout << "Integer" << endl;
-    } else {
-        cout << "Invalid Input" << endl;
+// Function to check if a given string is a floating point number
+bool is_float(const string& str) {
+    bool has_decimal = false;
+    for (int i = 0; i < str.length(); i++) {
+        if (isdigit(str[i])) {
+            continue;
+        } else if (str[i] == '.' && !has_decimal) {
+            has_decimal = true;
+        } else {
+            return false;
+        }
     }
-
-    return 0;
+    return has_decimal;
 }
