3af841ce5dd7330e873f380a8138eee0559340d9
Mohammad Al Faied <al15-4191@diu.edu.bd>
2024-11-07 23:09:48 +0600

    added

--- 02_Recognize_Comments.cpp
+++ 02_Recognize_Comments.cpp
@@ -7,3 +7,20 @@ int main() {
     string input;
     cout << "Enter a line of text: ";
     getline(cin, input);
+
+    if (input.size() >= 2) {
+        if (input[0] == '/' && input[1] == '/') {
+            cout << "Single line Comment" << endl;
+        } else if (input[0] == '/' && input[1] == '*') {
+            if (input[input.size() - 2] == '*' && input[input.size() - 1] == '/') {
+                string commentText = input.substr(2, input.size() - 4);
+                cout << "Multi-line Comment: " << commentText << endl;
+            } else {
+                cout << "Not a Comment" << endl;
+            }
+        } else {
+            cout << "Not a Comment" << endl;
+        }
+    }
+    return 0;
+}
